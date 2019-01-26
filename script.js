c156d064d37ac89443d637d38e694466be2dfbbf
Jessie Sarmiento <jessies@tywls-astoria.org>
2019-01-26 20:58:37 +0000

    Class_1

---
+++ script.js
@@ -0,0 +1,63 @@
+// alert('Hello World!');
+
+// const friendName ='Jazmin';
+// const instructorName = 'Tiffany';
+// const programName = 'NYOT';
+// const isCoding = true;
+// const numPizzaSlicesIAte = 1;
+// const numPizzaSlicesFriendAte = 2;
+
+// console.log('My Friend ' + friendName + ' is a student for ' + programName + ' in which she ate ' + numPizzaSlicesFriendAte +' slices of pizza.\n' + instructorName + ', our instructor, teaches at ' + programName + '.\n' + 'I ate ' + numPizzaSlicesIAte + ' slice of pizza due to how on average I eat only one.');
+
+// function parrotFacts(){
+//     console.log('Some parrot species can live over 80 years.');
+// }
+
+// parrotFacts();
+
+// function subtract(numOne, numTwo){
+//     console.log(numTwo - numOne);
+// }
+// subtract(3,2);
+
+// function callKitten(kittenName){
+//     console.log('Come here, ' + kittenName + '!');
+// }
+// callKitten('Fluffy');
+
+// function addNumbers(a,b){
+//     console.log(a + b);
+// }
+
+// addNumbers(5,7); //Output:12
+// addNumbers(9, 12); //Output:21
+
+// function sayHi(name){
+//     return "Hi, " + name;
+    
+// }
+
+// console.log(sayHi('Jeff'));
+
+// let cy = 2019;
+// let by = 2002;
+// let age1 = cy-by;
+// let age2 = age1 - 1;
+
+// function ageCalc(cy,by){
+// console.log('You can be '+ age1 + 'or ' + age2);
+
+
+// }
+
+// ageCalc();
+
+function ageClac(a,b){
+    let add = a + b;
+    let subtract = add - 1;
+}
+
+ageClac(2019,2002); 
+
+
+
